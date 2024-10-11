083d129c3fcf5825c66d59fc33cf8d7373a7ef06
Bettenoch <bettenoch254@gmail.com>
2024-10-11 14:45:56 +0300

    ADD: regoster user and login routes in api router.

--- app/api/main.py
+++ app/api/main.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from app.api.routes import home
+from app.api.routes import home, login, users
 
 api_router = APIRouter()
 
-
+api_router.include_router(login.router, tags=["login"])
 api_router.include_router(home.router, prefix="/dashboard", tags=["dashboard"])
+api_router.include_router(users.router, prefix="/users", tags=["users"])
